8f02294a60602c7fff649c7c55bfbe263286327f
Shreevatsa R <shreevatsa.public@gmail.com>
2022-04-24 09:01:36 -0700

    Implement Display for numeric object

--- src/lib.rs
+++ src/lib.rs
@@ -42,14 +42,16 @@ pub fn handle_file(file: File) -> u32 {
     crc32
 }
 
+// =====================
 // 7.3.2 Boolean Objects
+// =====================
 #[derive(PartialEq, Debug)]
 enum BooleanObject {
     True,
     False,
 }
 impl fmt::Display for BooleanObject {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(
             f,
             "{}",
@@ -89,11 +91,27 @@ fn parse_boolean_none() {
     assert!(err.is_err());
 }
 
+// =====================
 // 7.3.3 Numeric Objects
+// =====================
+// Store the sign separately, to be able to put it back.
 enum Sign {
     Plus,
     Minus,
     None,
+}
+impl fmt::Display for Sign {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Sign::Plus => "+",
+                Sign::Minus => "-",
+                Sign::None => "",
+            }
+        )
+    }
 }
 fn parse_sign(input: &str) -> IResult<&str, Sign> {
     let (input, sign) = opt(one_of("+-"))(input)?;
@@ -105,20 +123,35 @@ fn parse_sign(input: &str) -> IResult<&str, Sign> {
     };
     Ok((input, sign))
 }
-struct Integer {
+// Store the digits rather than just an i64, to be able to round-trip leading 0s.
+struct Integer<'a> {
     sign: Sign,
-    value: i64,
+    digits: &'a str,
+}
+impl fmt::Display for Integer<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}{}", self.sign, self.digits)
+    }
 }
 fn object_numeric_integer(input: &str) -> IResult<&str, Integer> {
-    let (input, (sign, value)) = tuple((parse_sign, digit1))(input)?;
-    let value = i64::from_str_radix(value, 10).unwrap();
-    Ok((input, Integer { sign, value }))
+    let (input, (sign, digits)) = tuple((parse_sign, digit1))(input)?;
+    // let value = i64::from_str_radix(digits, 10).unwrap();
+    Ok((input, Integer { sign, digits }))
 }
 
-struct Real {
+struct Real<'a> {
     sign: Sign,
-    digits_before: String,
-    digits_after: String,
+    digits_before: &'a str,
+    digits_after: &'a str,
+}
+impl fmt::Display for Real<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{}{}.{}",
+            self.sign, self.digits_before, self.digits_after
+        )
+    }
 }
 fn object_numeric_real(input: &str) -> IResult<&str, Real> {
     let (input, (sign, digits_before, _, digits_after)) = tuple((
@@ -131,15 +164,24 @@ fn object_numeric_real(input: &str) -> IResult<&str, Real> {
         input,
         Real {
             sign,
-            digits_before: digits_before.to_string(),
-            digits_after: digits_after.to_string(),
+            digits_before,
+            digits_after,
         },
     ))
 }
-enum NumericObject {
-    Integer(Integer),
-    Real(Real),
+enum NumericObject<'a> {
+    Integer(Integer<'a>),
+    Real(Real<'a>),
 }
+impl fmt::Display for NumericObject<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            NumericObject::Integer(i) => write!(f, "{}", i),
+            NumericObject::Real(r) => write!(f, "{}", r),
+        }
+    }
+}
+
 fn object_numeric(input: &str) -> IResult<&str, NumericObject> {
     let real = object_numeric_real(input);
     match real {
@@ -151,7 +193,11 @@ fn object_numeric(input: &str) -> IResult<&str, NumericObject> {
     }
 }
 
+// =====================
 // 7.3.4 String Objects
+// =====================
 
 // 7.3.4.2 Literal Strings
-// Sequence of non-\ bytes. (Note: When encoding, also need to escape unbalanced parentheses.)
+// Sequence of bytes, where only \ has special meaning. (Note: When encoding, also need to escape unbalanced parentheses.)
+// To be able to round-trip successfully, we'll store it as alternating sequences of <part before \, the special part after \>
+struct LiteralString {}
