dd183d7e598b72f9aad979347c52ce9e92c51f5f
Shreevatsa R <shreevatsa.public@gmail.com>
2022-05-19 14:55:00 -0700

    simplify comment

--- src/lib.rs
+++ src/lib.rs
@@ -17,27 +17,11 @@ use web_sys::{console, File, FileReaderSync};
 //     }
 // }
 
-// Consider a file containing three "characters": mकn
-// • m U+006D LATIN SMALL LETTER M
-// • क U+0915 DEVANAGARI LETTER KA
-// • n U+006E LATIN SMALL LETTER N
-// It is saved in UTF-8 as five bytes: 6d e0 a4 95 6e
-// But `readAsBinaryString` reads into a DOMString,
-// which is "a sequence of 16-bit unsigned integers":
-// in this case, a sequence of *FIVE* 16-bit integers.
-// These 5 integers, interpreted as USVs, would be:
-// • m U+006D LATIN SMALL LETTER M
-// • à U+00E0 LATIN SMALL LETTER A WITH GRAVE
-// • ¤ U+00A4 CURRENCY SIGN
-// • � U+0095 MESSAGE WAITING
-// • n U+006E LATIN SMALL LETTER N
-// Unfortunately, that sequence of "characters" is what gets encoded into UTF-8,
-// and the resulting 8 bytes are the (Rust) String we get from there.
-// But we can instead use `read_as_array_buffer`! See below.
 #[wasm_bindgen]
 pub fn handle_file(file: File) -> u32 {
     console::log_1(&format!("in Rust handle_file").into());
     let filereader = FileReaderSync::new().unwrap();
+    // Warning: This read_as_array_buffer can't be changed to readAsBinaryString.
     let buffer = filereader.read_as_array_buffer(&file).unwrap();
     let view = Uint8Array::new(&buffer); // This is instant.
     console::log_1(&format!("read {} bytes to ArrayBuffer", view.byte_length()).into());
